497edf9f3f79d3b4625e7fb8a76cd68483a7f162
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-04-18 22:08:33 +0200

    introduce pending step definitions

---
+++ features/steps/csp_steps.rb
@@ -0,0 +1,20 @@
+Given(/^I have a CSP problem$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I have a variable '\w+' with domain \[(.*)\]$/) do |variable, domain|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I have a constraint: .*$/) do |constraint|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I solve the problem$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I expect solutions$/) do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
